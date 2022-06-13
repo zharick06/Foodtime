107c0db9912abb80f559409e5de7be7e842684e0
zharick <zhakmar2@gmail.com>
2022-06-13 16:53:58 -0500

    make it better

--- FOOD_TIME/settings.py
+++ FOOD_TIME/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'FOOD_TIME',
         'USER': 'postgres',
-        'PASSWORD': 'FOODTIME123',
+        'PASSWORD': 'zharick0206',
         'HOST': 'localhost',
         'DATABASE_PORT': '5432'
     }
