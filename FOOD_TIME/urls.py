7390ead55aa1b7b8749109b47c9bc3c94af2cee4
Cohnante <101373160+Cohnante@users.noreply.github.com>
2022-06-15 06:56:52 -0500

    15/06/2022 pt10

--- FOOD_TIME/urls.py
+++ FOOD_TIME/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('',include('app.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls'), name='login'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
