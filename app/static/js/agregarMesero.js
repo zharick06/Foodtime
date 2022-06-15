89677b3b5509fd1adf950b78c7598b7cfb412ec2
Cohnante <101373160+Cohnante@users.noreply.github.com>
2022-06-15 07:36:20 -0500

    15/06/2022 pt25

--- app/static/js/agregarMesero.js
+++
@@ -1,9 +0,0 @@
-const labels = document.querySelectorAll('label')
-labels.forEach(element => {
-    console.log(element);
-    element.remove()
-});
-
-const nombre = document.getElementById("id_nombre")
-
-nombre.setAttribute("placeholder","Nombre")
