c64b42758baacc10a66cdeaf05411b9e7f29a69a
Cohnante <101373160+Cohnante@users.noreply.github.com>
2022-06-15 07:26:50 -0500

    15/06/2022 pt22

---
+++ static/js/login.js
@@ -0,0 +1,7 @@
+const inputPass = document.getElementById("id_password")
+
+inputPass.setAttribute("placeholder","Password")
+
+const inputUser = document.getElementById("id_username")
+
+inputUser.setAttribute("placeholder","Username")
